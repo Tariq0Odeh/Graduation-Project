09aef3aaf10718418d61c00545beec6e72199f28
Tariq Odeh <tariqodeh2001@gmail.com>
2023-11-14 16:32:00 +0200

    Update the slices creation

--- main.py
+++ main.py
@@ -1,56 +1,29 @@
 import numpy as np
-
-N = 5
-K = 2
-
-
-class Traffic:
-    def __init__(self):
-        self.data = np.random.randint(1, 10, N)
-
-    def __str__(self):
-        return f"Traffic: {self.data}"
-
-
-class Resources:
-    def __init__(self):
-        self.data = np.random.uniform(1.0, 10.0, K)
-
-    def __str__(self):
-        return f"Resources: {self.data}"
-
-
-class Qos:
-    def __init__(self):
-        self.data = np.random.uniform(1.0, 10.0, N)
-
-    def __str__(self):
-        return f"Qos: {self.data}"
+import pdb
 
 
 class Slice:
-    def __init__(self, t):
+    def __init__(self, t, k):
         self.t = t
-        self.x = [Traffic() for _ in range(t)]
-        self.r = [Resources() for _ in range(t)]
-        self.q = [Qos() for _ in range(t)]
-
-    def __str__(self):
-        return f"\n".join([f"\tDTI {i + 1}: {x}\n\t\t   {r}\n\t\t   {q}" for i, (x, r, q) in enumerate(zip(self.x, self.r, self.q))])
-
-
-def print_traffic_and_resource_matrices(slices, n_ttis):
-    i = 1
-    for slice in slices:
-        print(f"Slice - {i}")
-        print(slice)
-        i = i+1
+        self.x = np.random.randint(1, 10, len(t))
+        self.r = np.random.random(k)*10
+        self.q = np.random.random(len(t))*10
 
 
 if __name__ == '__main__':
-    M = 4
-    t = 3
 
-    slices = [Slice(t) for _ in range(M)]
+    N = 20  # Numbers of TTIs in DTI
+    K = 2  # Different types of resources allocated to slice
+    TTI = np.arange(1000)   # The transmission time interval
+    T = TTI[0::N]   # the set of starting points of all DTIs
+    qt = 8  # QoS minimum threshold
 
-    print_traffic_and_resource_matrices(slices, t)
+    for cnt in range(len(T)-1):
+        t = TTI[T[cnt]:(T[cnt+1])]
+        print(t)                #add here update function
+        slices = Slice(t, K)
+        print(slices.x)
+        print(slices.r)
+        print(slices.q)
+        pdb.set_trace()
+
