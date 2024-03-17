a49a3caa3fba621af2a1f167cd05ff822b1becc6
shahd <shahddaghash18@gmail.com>
2024-03-17 22:12:25 +0200

    Refactoring the name of variables/classes from slice to subnetwork.

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import pdb
 
 # Define global variables
 N = 20  # Numbers of TTIs in DTI
-K = 2  # Different types of resources allocated to slice
+K = 2  # Different types of resources allocated to subnetwork
 TTI = np.arange(1000)   # The transmission time interval
 T = TTI[0::N]   # the set of starting points of all DTIs
 qt = 6  # QoS minimum threshold
 
-# Used to initialize a slice
-class Slice:
+# Used to initialize a subnetwork
+class Subnetwork:
     def __init__(self, t):
         self.t = t
         self.x = np.random.randint(1, 10, len(t)) 
         self.r = np.random.random(K) * 10
         self.q = np.random.random(len(t)) * 10
 
-def betaCalculation(slice):
-    beta = np.sum((slice.q <= qt) * slice.x) / np.sum(slice.x)
+def betaCalculation(subnetwork):
+    beta = np.sum((subnetwork.q <= qt) * subnetwork.x) / np.sum(subnetwork.x)
     return beta
 
 # this function should be changed to get the actual traffic X
@@ -27,30 +27,28 @@ def getTraffic():
 
 # Use reinforcement learning to update the resounces
 def getResources():
-    beta = betaCalculation(slice) # calculate beta from previous results (q, x)
+    beta = betaCalculation(subnetwork) # calculate beta from previous results (q, x)
     return np.random.random(K) * 10
 
-# Function used to update a slice attributes
-def updateSlice(slice):
-    slice.x = getTraffic()
-    slice.r = getResources()
-    slice.q = np.random.random(len(t)) * 10
+# Function used to update a subnetwork attributes
+def updateSubnetwork(subnetwork):
+    subnetwork.x = getTraffic()
+    subnetwork.r = getResources()
+    subnetwork.q = np.random.random(len(t)) * 10
     
 if __name__ == '__main__':
-    slice = None
-    # slices creation
+    subnetwork = None
+    # subnetworks creation
     for cnt in range(len(T) - 1):
         t = TTI[T[cnt]:(T[cnt + 1])]
         print(t)    #add here update function
-        if slice == None: # create slice
-            slice = Slice(t)
-        else: # update slice
-            updateSlice(slice)
-        print("x = ", slice.x)
-        print("r = ", slice.r)
-        print("q = ", slice.q)
-        print("beta = ", betaCalculation(slice), "\n")
+        if subnetwork == None: # create subnetwork
+            subnetwork = Subnetwork(t)
+        else: # update subnetwork
+            updateSubnetwork(subnetwork)
+        print("x = ", subnetwork.x)
+        print("r = ", subnetwork.r)
+        print("q = ", subnetwork.q)
+        print("beta = ", betaCalculation(subnetwork), "\n")
 
         # pdb.set_trace()
-    
-# TODO: Verify what is the traffic in the first slice -> What it would be in the code
